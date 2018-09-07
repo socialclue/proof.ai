c89f849d742a231dd8b790bc0ea473552eb398f4
Shanky Rana <shankyrana@hotmail.com>
2018-09-08 01:43:37 +0530

    bull removed

--- api/websocket/services/Websocket.js
+++ api/websocket/services/Websocket.js
@@ -6,28 +6,6 @@
 const fs = require('fs');
 
 const webSocketStream = fs.createWriteStream('/tmp/log/websocket.log');
-var Queue = require('bull');
-var campaignQueue = new Queue('Data logger', 'redis://127.0.0.1:6379');
-campaignQueue.process(function(job, done){
-
-  // job.data contains the custom data passed when the job was created
-  // job.id contains id of this job.
-  console.log(job.data, '=======job');
-  // transcode video asynchronously and report progress
-  job.progress(42);
-
-  // call done when finished
-  done();
-
-  // or give a error if error
-  done(new Error('error transcoding'));
-
-  // or pass it a result
-  done(null, { framerate: 29.5 /* etc... */ });
-
-  // If the job throws an unhandled exception it is also handled correctly
-  throw new Error('some unexpected error');
-});
 
 
 module.exports =  {
@@ -40,7 +18,7 @@ module.exports =  {
     let message =  formatter + '\n';
     console.log(msg);
     if(msg.value && msg.value.event == 'formsubmit') {
-      campaignQueue.add({value: msg.value}, {priority:1});
+      // campaignQueue.add({value: msg.value}, {priority:1});
     }
     webSocketStream.write(message);
   },
