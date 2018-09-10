f0b45ff058ab35e23fa0e07b46d0e03360358531
Shanky Rana <shankyrana@hotmail.com>
2018-09-10 13:02:34 +0530

    redis host changed

--- config/functions/kue.js
+++ config/functions/kue.js
@@ -14,7 +14,7 @@ const q = kue.createQueue({
   prefix: 'q',
   redis: {
     port: strapi.config.redisPort,
-    host: 'redis',
+    host: '10.15.241.114',
     auth: strapi.config.redisPassword,
     db: strapi.config.redisDb, // if provided select a non-default redis db
     options: {
