4316657bf87f462393c10e25e17d3569a61cb3aa
Maek Twain <saranshisat@icloud.com>
2018-11-07 15:41:23 +0530

    Redis Config Update

--- config/functions/kue.js
+++ config/functions/kue.js
@@ -91,23 +91,10 @@ const redisStruct =
       auth: strapi.config.redisPassword,
       db: strapi.config.redisDb, // if provided select a non-default redis db
       options: {
-        retry_strategy: function( options ) {
-          if (options.error && options.error.code == 'ECONNREFUSED') {
-              return new Error('The server refused the connection');
-          }
-          if (options.total_retry_time > 1000 * 60 * 60) {
-            // End reconnecting after a specific timeout and flush all commands
-            // with a individual error
-            return new Error('Retry time exhausted');
-          }
-          if (options.attempt > 10) {
-            // End reconnecting with built in error
-            return undefined;
-          }
-          // reconnect after
-          return Math.min(options.attempt * 100, 3000);
+        retryStrategy: function (times) {
+          var delay = Math.min(times * 50, 2000);
+          return delay;
         }
-
       }
     };
 
