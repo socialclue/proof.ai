d4d27c886e4499e962adc39d293bd5c47154aea1
Saransh Sharma <marktwain@gmx.us>
2019-06-10 10:32:12 +0530

    Method Names

---
+++ config/functions/global.js
@@ -0,0 +1,44 @@
+// We define global methods here
+
+// We need to make sure that we have .env file before we start the server we start these services
+// Also ensuring that it should not delay.
+
+
+// A method to ensure that we have .env filebeat
+
+
+
+module.exports = async cb => {
+
+  const checkEnvFile = function() {
+    if (checkFile){
+
+    }
+
+  }
+
+  const setElasticClient = function (){
+
+  }
+
+  const setRedisClient = function() {
+
+  }
+
+  const getClient(name){
+    // It should get the client with respective names say "ES" so get es.client or say redis so we get redis.clients
+
+  }
+
+  const createEnvFile(){
+
+  }
+
+  const checkFile(){
+
+  }
+
+
+
+
+}
